bb699280ffefb576a58b54c6794edc9dd4533638
nmohan30 <nikithamohan@yahoo.com>
2018-11-05 15:35:49 -0800

    Create plot2.r

---
+++ plot2.r
@@ -0,0 +1,12 @@
+NEI[, Emissions := lapply(.SD, as.numeric), .SDcols = c("Emissions")]
+totalNEI <- NEI[fips=='24510', lapply(.SD, sum, na.rm = TRUE)
+                , .SDcols = c("Emissions")
+                , by = year]
+                
+subsetNEI  <- NEI[NEI$fips=="24510", ]
+
+aggregatedTotalByYear <- aggregate(Emissions ~ year, subsetNEI, sum)
+
+png('plot2.png')
+barplot(height=aggregatedTotalByYear$Emissions, names.arg=aggregatedTotalByYear$year, xlab="years", ylab=expression('total PM'[2.5]*' emission'),main=expression('Total PM'[2.5]*' in the Baltimore City, MD emissions at various years'))
+dev.off()
